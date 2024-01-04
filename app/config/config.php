003d0c483c3869dc4f1e4e52724fd4d0d3fc3ded
Iliass Raihani <43287534+Apter-X@users.noreply.github.com>
2024-01-04 17:33:52 +0100

    add config

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'api_rest';
+
+// App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+// URL Root
+const URL_ROOT = 'http://localhost/Kamikaze_Blog';
+// Site Name
+const SITE_NAME = 'Simple API';
